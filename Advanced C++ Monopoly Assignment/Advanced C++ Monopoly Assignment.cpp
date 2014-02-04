e943c5edb7822d89e9feb34e01075c4b1f6bdb92
ashm87 <amorris8@uclan.ac.uk>
2014-02-04 15:29:11 +0000

    Initial First Commit

---
+++ Advanced C++ Monopoly Assignment/Advanced C++ Monopoly Assignment.cpp
@@ -0,0 +1,13 @@
+// Advanced C++ Monopoly Assignment.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+const char POUND = 156;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
